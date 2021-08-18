b8fafd38356a5bfab569153553f9e95c9acbf30e
Bodnar Peter <bodnar.peter97@gmail.com>
2021-08-18 20:58:43 +0200

    base back-end and database connection

---
+++ back-end/server.js
@@ -0,0 +1,36 @@
+const dotenv = require('dotenv');
+const app = require('./app');
+const Sequelize = require('sequelize');
+
+dotenv.config({ path: './config.env' });
+
+const db = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+    host: process.env.DATABASE_HOST_URL,
+    dialect: 'mssql',
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+db.authenticate().then(() => {
+    console.log('Database connected...')
+}).catch(err => console.log(err));
+
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION!');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
