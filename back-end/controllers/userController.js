6d04ab0516bb24b8e76adaf8611ea42ec85d5774
Bodnar Peter <bodnar.peter97@gmail.com>
2021-08-21 13:58:20 +0200

    refactor

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -2,7 +2,6 @@ const factory = require('./handlerFactory');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const upload = require('./multerController');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -16,6 +15,7 @@ exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const UserRoles = require('../consts/userRoles');
 const userController = require('../controllers/userController');
 const authController = require('./../controllers/authController');
-const multerController = require('./../controllers/multerController');
+const imageUploadController = require('../controllers/imageUploadController');
 
 
 const router = express.Router();
@@ -14,7 +14,7 @@ router.get('/logout', authController.logout);
 router.use(authController.protect);
 
 router.get('/me', userController.getMe, userController.getUser)
-      .patch('/updateMe', multerController.uploadProfilePicture, userController.updateMe)
+      .patch('/updateMe', imageUploadController.uploadProfilePicture, userController.updateMe)
 
 router.use(authController.restrictTo('admin'));
 
