d5a3e6994191b6128c4abe587dd76cc1aaf8efa4
Bodnar Peter <bodnar.peter97@gmail.com>
2021-08-22 21:11:36 +0200

    add route guards

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { UsersComponent } from './components/users/users.component';
+import { AdminPermissionGuard } from './guards/admin-permission.guard';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'signin' },
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'users', component: UsersComponent }
+  { path: 'users', component: UsersComponent, canActivate: [AuthGuard, AdminPermissionGuard]}
 ];
 
 @NgModule({
